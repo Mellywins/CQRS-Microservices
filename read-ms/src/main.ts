6e7f1d47151c31ba9e31119b67151e65fce04a77
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-11-15 21:09:51 +0100

    init

---
+++ read-ms/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,
+    {
+      transport:Transport.TCP
+    });
+   app.listen();
+}
+bootstrap();
---
+++ readme.md
@@ -0,0 +1,1 @@
+# CQRS Project Scaffolded sandbox
