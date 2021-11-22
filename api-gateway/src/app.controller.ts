5a596bc7a4c94c73915f0ce53f314af9202b6da3
Oussema <oussemazouaghi@insat.u-carthage.tn>
2021-11-22 18:37:17 +0100

    using events instend of TCP messages for communication

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -4,9 +4,13 @@ import {
   ClassSerializerInterceptor,
   UseInterceptors,
   Inject,
+  Post,
+  Body,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ClientProxy } from '@nestjs/microservices';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller()
 @UseInterceptors(ClassSerializerInterceptor)
@@ -16,6 +20,14 @@ export class AppController {
     @Inject('WRITE_USER_SERVICE') private writeUserService: ClientProxy,
   ) {}
 
+  @Post('create')
+  async createUser(@Body() createUserPayload: CreateUserDto) {
+    return this.writeUserService.emit('create_user', createUserPayload);
+  }
+  @Post('update')
+  async updateUser(@Body() updateUserPayload: UpdateUserDto) {
+    return this.writeUserService.emit('update_user', updateUserPayload);
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- write-ms/src/user/dto/create-user.dto.ts
+++ write-ms/src/user/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 export class CreateUserDto {
-    readonly username: string;
-    readonly password: string;
-    readonly email: string;
+  readonly username: string;
+  readonly password: string;
+  readonly email: string;
 }
